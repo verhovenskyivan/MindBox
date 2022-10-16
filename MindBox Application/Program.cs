9f90c5dc52c5930800a442680cdf0eb12fdd92e1
verhovensky <verhovenskyivan@gmail.com>
2022-10-16 17:17:42 +0300

    added new application

---
+++ MindBox Application/Program.cs
@@ -0,0 +1,30 @@
+﻿using MindBox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace MindBox
+{
+    public class Circle
+    {
+        public Circle(double Radius)
+        {
+            if (Radius < 0)
+            {
+                throw new ArgumentException("Недопустимое значение радиуса. ");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите радиус: ");
+            double Radius = Convert.ToDouble(Console.ReadLine());
+            double Area = Math.PI * Radius * Radius; Console.WriteLine("Площадь круга: " + Area);
+            Console.ReadKey();
+        }
+    }
+}
